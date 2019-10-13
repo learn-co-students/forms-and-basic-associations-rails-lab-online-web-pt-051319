905f0d0dff512fc680273cd3c020bf404e21f0c6
Jasmine <jasmine1226@gmail.com>
2019-10-13 16:35:19 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,11 +43,12 @@ class SongsController < ApplicationController
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
-
+  
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
+
 end
 
